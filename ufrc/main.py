e022e9fe976040f1b7255f5990936857f104f06b
Nick DeRobertis <whoopnip@gmail.com>
2022-01-15 18:36:51 -0500

    Run commands in a shell so that PATH is set correctly

--- ufrc/main.py
+++ ufrc/main.py
@@ -1,8 +1,10 @@
 import os
+import re
 import tempfile
 from pathlib import Path
-from typing import Final, Optional, Sequence, Union
+from typing import Final, Optional, Sequence, Union, List
 
+import paramiko
 import scp
 from scp import SCPClient
 
@@ -24,7 +26,11 @@ class UFRC:
     def connect(self, username: str, password: str):
         ssh = SSHClient()
         ssh.load_system_host_keys()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(self.server, username=username, password=password)
+        channel = ssh.invoke_shell()
+        self._stdin = channel.makefile("wb")
+        self._stdout = channel.makefile("r")
         self._ssh_client = ssh
         self._scp_client = SCPClient(ssh.get_transport())
 
@@ -39,17 +45,60 @@ class UFRC:
     def is_connected(self) -> bool:
         return self._ssh_client is not None
 
+    def _execute_command(self, cmd: str) -> SSHResponse:
+        # Adapted from https://stackoverflow.com/a/36948840/6276321
+        cmd = cmd.strip("\n")
+        self._stdin.write(cmd + "\n")
+        finish = "end of stdOUT buffer. finished with exit status"
+        echo_cmd = "echo {} $?".format(finish)
+        self._stdin.write(echo_cmd + "\n")
+        shin = self._stdin
+        self._stdin.flush()
+
+        shout: List[str] = []
+        sherr: List[str] = []
+        exit_status = 0
+        for line in self._stdout:
+            if str(line).startswith(cmd) or str(line).startswith(echo_cmd):
+                # up for now filled with shell junk from stdin
+                shout = []
+            elif str(line).startswith(finish):
+                # our finish command ends with the exit status
+                exit_status = int(str(line).rsplit(maxsplit=1)[1])
+                if exit_status:
+                    # stderr is combined with stdout.
+                    # thus, swap sherr with shout in a case of failure.
+                    sherr = shout
+                    shout = []
+                break
+            else:
+                # get rid of 'coloring and formatting' special characters
+                shout.append(
+                    re.compile(r"(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]")
+                    .sub("", line)
+                    .replace("\b", "")
+                    .replace("\r", "")
+                )
+
+        # first and last lines of shout/sherr contain a prompt
+        if shout and echo_cmd in shout[-1]:
+            shout.pop()
+        if shout and cmd in shout[0]:
+            shout.pop(0)
+        if sherr and echo_cmd in sherr[-1]:
+            sherr.pop()
+        if sherr and cmd in sherr[0]:
+            sherr.pop(0)
+
+        return SSHResponse(stdout="\n".join(shout), stderr="\n".join(sherr))
+
     def run(self, command: str) -> SSHResponse:
         if self._ssh_client is None:
             raise NoUFRCConnectionException(
                 "Use .connect(username, password) before .run"
             )
 
-        _, ssh_stdout, ssh_stderr = self._ssh_client.exec_command(command)
-        return SSHResponse(
-            stdout=ssh_stdout.read().decode(),
-            stderr=ssh_stderr.read().decode(),
-        )
+        return self._execute_command(command)
 
     def put(
         self,
